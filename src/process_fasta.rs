7b8ac9e521ceaeeed47c364eb54e9ae4022c57e6
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2023-10-16 14:46:09 +0300

    experiment refactor, statistical funcs implemented

--- src/process_fasta.rs
+++ src/process_fasta.rs
@@ -6,6 +6,33 @@ use std::time::Instant;
 use seq_io::fasta::Record;
 use rust_lcp;
 
+
+fn mean(data: &[usize]) -> Option<f32> {
+    let sum = data.iter().sum::<usize>() as f32;
+    let count = data.len();
+
+    match count {
+        positive if positive > 0 => Some(sum / count as f32),
+        _ => None,
+    }
+}
+
+
+fn std_deviation(data: &[usize]) -> Option<f32> {
+    match (mean(data), data.len()) {
+        (Some(data_mean), count) if count > 0 => {
+            let variance = data.iter().map(|value| {
+                let diff = data_mean - (*value as f32);
+
+                diff * diff
+            }).sum::<f32>() / count as f32;
+
+            Some(variance.sqrt())
+        },
+        _ => None
+    }
+}
+
   
 pub fn process_fasta(fasta_path: &str) {
     
@@ -37,44 +64,61 @@ pub fn process_fasta(fasta_path: &str) {
         println!("Total number of cores: {}", lcp_str.cores.len());
 
         for i in 1..5 {
-            let start = Instant::now();
+            let start_level = Instant::now();
             
             lcp_str.deepen();
 
             println!("End of deepeneing to level: {}", i);
 
-            let mut prev_index: usize = lcp_str.cores[0].end;
-            let mut overlapping_count: usize = 0;
-            let mut distances = [0 as usize; 10000];
-            let mut lengths = [0 as usize; 10000];
-            for (index, core) in lcp_str.cores.iter().enumerate().skip(1) {                
-                if core.start < prev_index {
-                    overlapping_count += 1;
-                } else {
-                    if core.start-prev_index < 10000 {
-                        distances[core.start-prev_index] += 1;
+            if lcp_str.cores.len() > 0 {
+                let mut prev_index_end: usize = lcp_str.cores[0].end;
+                let mut prev_index_start: usize = lcp_str.cores[0].start;
+                let mut overlapping_count: usize = 0;
+                let mut distances_pos = [0 as usize; 10000];
+                let mut distances = [0 as usize; 10000];
+                let mut lengths = [0 as usize; 10000];
+                for (index, core) in lcp_str.cores.iter().enumerate().skip(1) {                
+                    if core.start < prev_index_end {
+                        overlapping_count += 1;
                     } else {
-                        println!("Found large distance in level {}, core index: {}, core.start: {}, core.end: {}", i, index, core.start, core.end)
+                        if core.start-prev_index_end < 10000 {
+                            distances[core.start-prev_index_end] += 1;
+                        } else {
+                            println!("Found large distance in level {}, core index: {}, core.start: {}, core.end: {}, prev_core.start: {}, prev_core.end: {}", i, index, core.start, core.end, prev_index_start, prev_index_end)
+                        }
                     }
-                }
-                
-                if core.end-core.start < 10000 {
-                    lengths[core.end-core.start] += 1;
-                } else {
-                    println!("Found large length in level {}, core index: {}, core.start: {}, core.end: {}", i, index, core.start, core.end)
+                    
+                    if core.start-prev_index_start < 10000 {
+                        distances_pos[core.start-prev_index_start] += 1;
+                    } else {
+                        println!("Found large distance between start pos in level {}, core index: {}, core.start: {}, core.end: {}, prev_core.start: {}, prev_core.end: {}", i, index, core.start, core.end, prev_index_start, prev_index_end)
+                    }
+                    
+                    if core.end-core.start < 10000 {
+                        lengths[core.end-core.start] += 1;
+                    } else {
+                        println!("Found large length in level {}, core index: {}, core.start: {}, core.end: {}", i, index, core.start, core.end)
+                    }
+
+                    prev_index_end = core.end;
+                    prev_index_start = core.start;
                 }
 
-                prev_index = core.end;
+                println!("Level: {}", i);
+                println!("Overlapping core counts: {}", overlapping_count);
+                println!("Std of distances btw cores: {:?}", std_deviation(&distances));
+                println!("Mean of disntances btw cores: {:?}", mean(&distances));
+                println!("Std of distances btw starts: {:?}", std_deviation(&distances_pos));
+                println!("Mean of disntances btw starts: {:?}", mean(&distances_pos));
+                println!("Std of lengths: {:?}", std_deviation(&lengths));
+                println!("Mean of lengths: {:?}", mean(&lengths));
+                // println!("Distances: {:?}", distances);
+                // println!("Lengths: {:?}", lengths);
             }
-
-            let duration = start.elapsed();
-            println!("Total execution time: {:?}", duration);
+            
+            let duration_level = start_level.elapsed();
+            println!("Total execution time: {:?}", duration_level);
             println!("Total number of cores: {}", lcp_str.cores.len());
-
-            println!("Level: {}", i);
-            println!("Overlapping core counts: {}", overlapping_count);
-            println!("Distances: {:?}", distances);
-            println!("Lengths: {:?}", lengths);
             println!();
         }
 
